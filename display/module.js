06b2d84f0dee7abebbbbb712135a3f0317fc1ee0
Dawson Toth <dawson@tothsolutions.com>
2018-11-21 11:25:59 -0500

    Tweak Colors in Display

--- display/module.js
+++ display/module.js
@@ -1,4 +1,5 @@
 const electron = require('electron'),
+	_ = require('lodash'),
 	{ BrowserWindow } = electron;
 
 let displayWindow;
@@ -8,26 +9,29 @@ function createDisplayWindow() {
 	let screen = electron.screen,
 		secondaryDisplay = screen
 			.getAllDisplays()
-			.find(display => display.bounds.x || display.bounds.y);
-	if (secondaryDisplay) {
-		displayWindow = exports.window = new BrowserWindow({
-			frame: false,
-			alwaysOnTop: true,
-			// fullscreen: true,
-			title: 'Lower Thirds',
-			backgroundColor: '#0000ff',
-			// titleBarStyle: 'customButtonsOnHover',
-			// x: 200,
-			// y: 30,
-			// width: 192 * 2,
-			// height: 108 * 2
-			x: secondaryDisplay.bounds.x,
-			y: secondaryDisplay.bounds.y,
-			width: secondaryDisplay.bounds.width,
-			height: secondaryDisplay.bounds.height
-		});
-		displayWindow.loadFile('display/index.html');
-		// displayWindow.webContents.openDevTools();
-		displayWindow.on('closed', () => displayWindow = null);
-	}
+			.find(display => display.bounds.x || display.bounds.y),
+		position = secondaryDisplay
+			? {
+				x: secondaryDisplay.bounds.x,
+				y: secondaryDisplay.bounds.y,
+				width: secondaryDisplay.bounds.width,
+				height: secondaryDisplay.bounds.height
+			}
+			: {
+				x: 200,
+				y: 30,
+				width: 192 * 2,
+				height: 108 * 2
+			};
+	displayWindow = exports.window = new BrowserWindow(_.defaults(position, {
+		frame: false,
+		alwaysOnTop: true,
+		// fullscreen: true,
+		title: 'Lower Thirds',
+		backgroundColor: '#000000',
+		// titleBarStyle: 'customButtonsOnHover'
+	}));
+	displayWindow.loadFile('display/index.html');
+	// displayWindow.webContents.openDevTools();
+	displayWindow.on('closed', () => displayWindow = null);
 }
