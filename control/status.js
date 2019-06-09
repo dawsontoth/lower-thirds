8e61762b69fadc5be669fadfaf8169d8e97133bf
Dawson Toth <dawson@tothsolutions.com>
2019-06-09 12:34:15 -0400

    Fix IP discovery

--- control/status.js
+++ control/status.js
@@ -1,18 +1,22 @@
 let cp = require('mz/child_process'),
     fetch = require('node-fetch'),
-    async = require('async');
+    async = require('async'),
+    store = require('../lib/store'),
+    ip = require('ip'),
+    os = require('os'),
+    net = require('net');
 
-const TEN_MEGA_BYTE = 1024 * 1024 * 10;
-const ONE_MINUTE = 60 * 1000;
-const options = {
-    maxBuffer: TEN_MEGA_BYTE,
-    timeout: ONE_MINUTE
+let options = {
+    maxBuffer: 1024 * 1024 * 10,
+    timeout: 60 * 1000
 };
 
-let status = document.getElementById('status');
-let cerevoIP;
-let scanning = false;
-let recording = false;
+let status = document.getElementById('status'),
+    cerevoIP = store.get('last-cerevo-ip', null),
+    scanning = false,
+    recording = false;
+
+Promise.all(getServers().map(pingServer));
 status.onclick = onToggleStatus;
 setInterval(ensureConnected, 2000);
 
@@ -33,7 +37,8 @@ function scan() {
     scanning = true;
     cp.exec('arp -a', options)
         .then(data => {
-            const ips = String(data).match(/10\.3\.\d{1,3}\.\d{1,3}/g);
+            const ips = String(data).match(/10\.3\.\d{1,3}\.\d{1,3}/g).sort().reverse();
+            console.log(ips);
             async.eachLimit(
                 ips,
                 10,
@@ -41,6 +46,7 @@ function scan() {
                     if (await test(ip)) {
                         // No error thrown? Then we connected!
                         cerevoIP = ip;
+                        store.set('last-cerevo-ip', cerevoIP);
                         status.classList.add('connected');
                         throw new Error('Stop trying to connect to others');
                     }
@@ -79,8 +85,48 @@ function hit(ip, payload) {
                 status.classList.add('recording');
             }
             else {
-                status.classList.remove('recording');  
+                status.classList.remove('recording');
             }
             return json;
         });
+}
+
+function getServers() {
+    var interfaces = os.networkInterfaces();
+    var result = [];
+
+    for (var key in interfaces) {
+        var addresses = interfaces[key];
+        for (var i = addresses.length; i--;) {
+            var address = addresses[i];
+            if (address.family === 'IPv4' && !address.internal) {
+                var subnet = ip.subnet(address.address, address.netmask);
+                var current = ip.toLong(subnet.firstAddress);
+                var last = ip.toLong(subnet.lastAddress) - 1;
+                while (current++ < last) {
+                    result.push(ip.fromLong(current));
+                }
+            }
+        }
+    }
+
+    return result
+}
+
+function pingServers() {
+    return Promise.all(servers.map(pingServer));
+}
+
+function pingServer(address) {
+    return new Promise(function (resolve) {
+        var socket = new net.Socket();
+        socket.setTimeout(1000, close);
+        socket.connect(80, address, close);
+        socket.once('error', close);
+
+        function close() {
+            socket.destroy();
+            resolve(address);
+        }
+    });
 }
