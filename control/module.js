9f75ed566ee9e1cf55f28a121ec6ba4eaa2202eb
Dawson Toth <dawson@tothsolutions.com>
2020-02-22 21:46:51 -0500

    chore: Remove lodash

--- control/module.js
+++ control/module.js
@@ -1,8 +1,8 @@
 const electron = require('electron'),
-	testing = require('../config').testing,
-	_ = require('lodash'),
-	{ BrowserWindow, app, globalShortcut } = electron,
-	store = new (require('../lib/store'))('control');
+    testing = require('../config').testing,
+    {BrowserWindow, app, globalShortcut} = electron,
+    shallowDefaults = require('../lib/defaults'),
+    store = new (require('../lib/store'))('control');
 
 let controlWindow;
 exports.init = createControlWindow;
@@ -10,65 +10,64 @@ app.on('ready', registerHotKey);
 app.on('activate', activate);
 
 function registerHotKey() {
-	let ret = globalShortcut.register('CommandOrControl+D', hotKeyPressed);
-	if (!ret) {
-		console.error('registration failed');
-	}
+    let ret = globalShortcut.register('CommandOrControl+D', hotKeyPressed);
+    if (!ret) {
+        console.error('registration failed');
+    }
 }
 
 function hotKeyPressed() {
-	if (controlWindow) {
-		controlWindow.focus();
-	}
+    if (controlWindow) {
+        controlWindow.focus();
+    }
 }
 
 function activate() {
-	if (controlWindow === null) {
-		createControlWindow();
-	}
+    if (controlWindow === null) {
+        createControlWindow();
+    }
 }
 
 function createControlWindow() {
-	let defaults = {
-			autoHideMenuBar: true,
-			x: 200,
-			y: 30,
-			width: 400,
-			height: 200,
-			title: 'Lower Thirds',
-			backgroundColor: '#000000',
-			acceptFirstMouse: true,
-			webPreferences: {
-				nodeIntegration: true,
-			},
-		},
-		position = store.get('control-window-position') || defaults;
-	if (position.width < defaults.width) {
-		position.width = defaults.width;
-	}
-	if (position.height < defaults.height) {
-		position.height = defaults.height;
-	}
-	controlWindow = exports.window = new BrowserWindow(_.defaults(position, defaults));
-	controlWindow.on('resize', saveState);
-	controlWindow.on('move', saveState);
-	if (testing) {
-		controlWindow.loadURL('http://localhost:3000');
-	}
-	else {
-		controlWindow.loadFile('control/build/index.html');
-	}
-	// controlWindow.maximize();
-	// controlWindow.setFullScreen(true);
-	controlWindow.on('closed', () => electron.app.quit());
+    let defaults = {
+            autoHideMenuBar: true,
+            x: 200,
+            y: 30,
+            width: 400,
+            height: 200,
+            title: 'Lower Thirds',
+            backgroundColor: '#000000',
+            acceptFirstMouse: true,
+            webPreferences: {
+                nodeIntegration: true,
+            },
+        },
+        position = store.get('control-window-position') || defaults;
+    if (position.width < defaults.width) {
+        position.width = defaults.width;
+    }
+    if (position.height < defaults.height) {
+        position.height = defaults.height;
+    }
+    controlWindow = exports.window = new BrowserWindow(shallowDefaults(position, defaults));
+    controlWindow.on('resize', saveState);
+    controlWindow.on('move', saveState);
+    if (testing) {
+        controlWindow.loadURL('http://localhost:3000');
+    } else {
+        controlWindow.loadFile('control/build/index.html');
+    }
+    // controlWindow.maximize();
+    // controlWindow.setFullScreen(true);
+    controlWindow.on('closed', () => electron.app.quit());
 }
 
 function saveState() {
-	let bounds = controlWindow.getBounds();
-	store.set('control-window-position', {
-		x: bounds.x,
-		y: bounds.y,
-		width: bounds.width,
-		height: bounds.height,
-	});
+    let bounds = controlWindow.getBounds();
+    store.set('control-window-position', {
+        x: bounds.x,
+        y: bounds.y,
+        width: bounds.width,
+        height: bounds.height,
+    });
 }
--- display/module.js
+++ display/module.js
@@ -1,52 +1,51 @@
 const electron = require('electron'),
-	testing = require('../config').testing,
-	_ = require('lodash'),
-	{ BrowserWindow } = electron;
+    testing = require('../config').testing,
+    shallowDefaults = require('../lib/defaults'),
+    {BrowserWindow} = electron;
 
 exports.windows = [];
 exports.init = createDisplayWindow;
 
 function createDisplayWindow(alphaChannel) {
-	let screen = electron.screen,
-		displays = screen.getAllDisplays().sort((a, b) => a.bounds.x - b.bounds.x),
-		secondaryDisplay = displays.length > 2 && displays[displays.length - 2],
-		tertiaryDisplay = displays.length > 2 && displays[displays.length - 1],
-		displayOn = alphaChannel ? tertiaryDisplay : secondaryDisplay,
-		position = displayOn
-			? {
-				x: displayOn.bounds.x,
-				y: displayOn.bounds.y,
-				width: displayOn.bounds.width,
-				height: displayOn.bounds.height,
-			}
-			: {
-				x: 200 + (!alphaChannel ? 0 : 192 * 2),
-				y: 30,
-				width: 192 * 2,
-				height: 108 * 2,
-			};
-	if (!testing && alphaChannel && (!secondaryDisplay || !tertiaryDisplay)) {
-		return;
-	}
-	let displayWindow = new BrowserWindow(_.defaults(position, {
-		frame: false,
-		alwaysOnTop: true,
-		focusable: true,
-		fullscreen: false,
-		title: 'Titles - ' + (alphaChannel ? 'Alpha' : 'Display'),
-		backgroundColor: '#000',
-		webPreferences: {
-			nodeIntegration: true,
-		},
-	}));
-	if (testing) {
-		displayWindow.loadURL('http://localhost:3001');
-	}
-	else {
-		displayWindow.loadFile('display/build/index.html');
-	}
-	displayWindow.webContents.executeJavaScript(
-		`document.body.classList.add("${alphaChannel ? 'for' : 'not'}-alpha-channel")`,
-	);
-	exports.windows.push(displayWindow);
+    let screen = electron.screen,
+        displays = screen.getAllDisplays().sort((a, b) => a.bounds.x - b.bounds.x),
+        secondaryDisplay = displays.length > 2 && displays[displays.length - 2],
+        tertiaryDisplay = displays.length > 2 && displays[displays.length - 1],
+        displayOn = alphaChannel ? tertiaryDisplay : secondaryDisplay,
+        position = displayOn
+            ? {
+                x: displayOn.bounds.x,
+                y: displayOn.bounds.y,
+                width: displayOn.bounds.width,
+                height: displayOn.bounds.height,
+            }
+            : {
+                x: 200 + (!alphaChannel ? 0 : 192 * 2),
+                y: 30,
+                width: 192 * 2,
+                height: 108 * 2,
+            };
+    if (!testing && alphaChannel && (!secondaryDisplay || !tertiaryDisplay)) {
+        return;
+    }
+    let displayWindow = new BrowserWindow(shallowDefaults(position, {
+        frame: false,
+        alwaysOnTop: true,
+        focusable: true,
+        fullscreen: false,
+        title: 'Titles - ' + (alphaChannel ? 'Alpha' : 'Display'),
+        backgroundColor: '#000',
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    }));
+    if (testing) {
+        displayWindow.loadURL('http://localhost:3001');
+    } else {
+        displayWindow.loadFile('display/build/index.html');
+    }
+    displayWindow.webContents.executeJavaScript(
+        `document.body.classList.add("${alphaChannel ? 'for' : 'not'}-alpha-channel")`,
+    );
+    exports.windows.push(displayWindow);
 }
